f54bf687628bddf1591bcc1bbb895a9da312523d
sfaragy <solimankhulna@gmail.com>
2024-05-06 19:56:41 +0700

    Refactor code cleanup using Lombok and remove unwanted DTO attribute.

--- src/main/java/com/minden/exercise/mindenrestapi/dto/CourseRegistrationDto.java
+++ src/main/java/com/minden/exercise/mindenrestapi/dto/CourseRegistrationDto.java
@@ -1,40 +1,18 @@
 package com.minden.exercise.mindenrestapi.dto;
 
-import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class CourseRegistrationDto {
 
     private Long courseId;
     private Long studentId;
-    private Date registrationDate;
 
-    public CourseRegistrationDto(Long courseId, Long studentId, Date registrationDate) {
+    public CourseRegistrationDto(Long courseId, Long studentId) {
         this.courseId = courseId;
         this.studentId = studentId;
-        this.registrationDate = registrationDate;
-    }
-    public void setCourseId(Long courseId) {
-        this.courseId = courseId;
-    }
-
-    public void setStudentId(Long studentId) {
-        this.studentId = studentId;
-    }
-
-    public void setRegistrationDate(Date registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public Long getCourseId() {
-        return courseId;
-    }
-
-    public Long getStudentId() {
-        return studentId;
-    }
-
-    public Date getRegistrationDate() {
-        return registrationDate;
     }
 
 }

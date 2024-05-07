8fd2c42c91299b33467ff28e1429b45a769185ce
sfaragy <solimankhulna@gmail.com>
2024-05-07 13:16:47 +0700

    Refactor DTO for course registration

--- src/main/java/com/minden/exercise/mindenrestapi/dto/CourseRegistrationDto.java
+++ src/main/java/com/minden/exercise/mindenrestapi/dto/CourseRegistrationDto.java
@@ -1,18 +1,27 @@
 package com.minden.exercise.mindenrestapi.dto;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class CourseRegistrationDto {
 
+    @NonNull
     private Long courseId;
+
+    @NonNull
     private Long studentId;
 
-    public CourseRegistrationDto(Long courseId, Long studentId) {
+
+    public CourseRegistrationDto()
+    {
+
+    }
+
+    public CourseRegistrationDto(@NonNull Long courseId, @NonNull Long studentId) {
         this.courseId = courseId;
         this.studentId = studentId;
     }
-
 }
